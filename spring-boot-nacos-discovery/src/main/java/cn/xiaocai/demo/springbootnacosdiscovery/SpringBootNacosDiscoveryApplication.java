47c2489bc426043c836165ceed45fefb22f61f86
small-rose <small-rose@qq.com>
2021-04-30 16:12:16 +0800

    spring-boot-rabbitmq整合

---
+++ spring-boot-nacos-discovery/src/main/java/cn/xiaocai/demo/springbootnacosdiscovery/SpringBootNacosDiscoveryApplication.java
@@ -0,0 +1,18 @@
+package cn.xiaocai.demo.springbootnacosdiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author  zhangxiaocai.cn
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SpringBootNacosDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootNacosDiscoveryApplication.class, args);
+    }
+
+}
---
+++ spring-boot-nacos-discovery/src/test/java/cn/xiaocai/demo/springbootnacosdiscovery/SpringBootNacosDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.xiaocai.demo.springbootnacosdiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootNacosDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
