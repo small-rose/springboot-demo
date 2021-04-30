2a67ca202c0d1e80b0ff1cd5b49de56ea7b4ac0d
small-rose <small-rose@qq.com>
2021-04-30 16:13:43 +0800

    spring-boot-nacos-discovery整合

--- spring-boot-nacos-discovery/src/main/java/cn/xiaocai/demo/springbootnacosdiscovery/SpringBootNacosDiscoveryApplication.java
+++ spring-boot-nacos-discovery/src/main/java/cn/xiaocai/demo/springbootnacosdiscovery/SpringBootNacosDiscoveryApplication.java
@@ -12,6 +12,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class SpringBootNacosDiscoveryApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SpringBootNacosDiscoveryApplication.class, args);
     }
 
