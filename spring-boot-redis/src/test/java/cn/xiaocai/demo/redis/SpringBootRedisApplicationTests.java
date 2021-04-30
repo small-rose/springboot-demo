91e6fd6996287b282ca4d189dfe27aff4bfd361c
small-rose <small-rose@qq.com>
2021-04-30 15:02:48 +0800

    spring-boot-redis整合

---
+++ spring-boot-redis/src/test/java/cn/xiaocai/demo/redis/SpringBootRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.xiaocai.demo.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
