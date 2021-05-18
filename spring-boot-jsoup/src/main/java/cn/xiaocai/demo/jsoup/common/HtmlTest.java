50136a0e8e2c75e8addb92a7d3e91c0abef080f5
small-rose <small-rose@qq.com>
2021-05-18 23:06:07 +0800

    spring-boot jsoup 测试

--- spring-boot-jsoup/src/main/java/cn/xiaocai/demo/jsoup/common/HtmlTest.java
+++ spring-boot-jsoup/src/main/java/cn/xiaocai/demo/jsoup/common/HtmlTest.java
@@ -15,12 +15,17 @@ import org.jsoup.select.Elements;
 public class HtmlTest {
 
     static final String dir = "C:\\Users\\Dell-170020\\Desktop\\";
-    //static String url = "http://www.xuexi.la/xueke/708076.html";
-    static String url = "http://www.xuexi.la/xueke/703349.html";
+    static String url_daodeyufazhi = "https://doc.docsou.com/b75dceb56dc80c10493ba132ab34f4cb12464c0f6.html";
 
     static final String FILE_NAME = "";
 
-    public static void main(String[] args) {
+    public static void handle1(){
+        String url = "http://www.xuexi.la/xueke/708076.html";
+        getDocuent(url);
+
+    }
+
+    private static void getDocuent(String url) {
         Document document  = NetUtil.getDocument(url);
         Elements eles = document.select("#contentText");
         for (Element ele: eles) {
@@ -31,6 +36,29 @@ public class HtmlTest {
             }
             System.out.println(x);
         }
-
     }
+
+    public static void handle2(String url){
+        Document document  = NetUtil.getDocument(url);
+        Elements eles = document.select(".pages .next");
+        for (Element element :eles){
+            String href = element.attr("href");
+            System.out.println(element);
+            System.out.println(href);
+        }
+        for (Element ele: eles) {
+            String x = ele.select("p").html();
+            if (x.contains("strong")){
+                x = x.replaceAll("<strong>","");
+                x = x.replaceAll("</strong>","");
+            }
+            System.out.println(x);
+        }
+    }
+
+    public static void main(String[] args) {
+        handle2(url_daodeyufazhi);
+    }
+
+
 }
