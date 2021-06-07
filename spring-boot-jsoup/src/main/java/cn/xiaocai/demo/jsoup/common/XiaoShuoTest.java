e6636dd7f75834b6b49086f4aaed2cf22d2ec2ec
small-rose <small-rose@qq.com>
2021-06-07 17:04:13 +0800

    htmlUnit

---
+++ spring-boot-jsoup/src/main/java/cn/xiaocai/demo/jsoup/common/XiaoShuoTest.java
@@ -0,0 +1,30 @@
+package cn.xiaocai.demo.jsoup.common;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+/**
+ * @program: springboot-jsoup
+ * @description
+ * @function:
+ * @author: zzy
+ * @create: 2021-06-07 16:17
+ **/
+public class XiaoShuoTest {
+
+    public static void main(String[] args) {
+        String url = "https://weread.qq.com/web/reader/4de326a0721770aa4de95f4";
+        url = "https://weread.qq.com/web/reader/4de326a0721770aa4de95f4kc4c329b011c4ca4238a0201";
+        HtmlUtil htmlUtil = new HtmlUtil();
+        String html = htmlUtil.getHtml(url);
+        // System.out.println(html);
+        Document document = Jsoup.parse(html);
+        System.out.println(document);
+        handlerDoc(document);
+    }
+
+    private static void handlerDoc(Document document) {
+
+
+    }
+}
