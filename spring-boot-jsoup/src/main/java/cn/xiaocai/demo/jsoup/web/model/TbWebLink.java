cf04b9237684dabaaa6b04654f5f035085bc4717
small-rose <small-rose@qq.com>
2021-12-23 22:58:11 +0800

    add entity

---
+++ spring-boot-jsoup/src/main/java/cn/xiaocai/demo/jsoup/web/model/TbWebLink.java
@@ -0,0 +1,72 @@
+package cn.xiaocai.demo.jsoup.web.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+/**
+ * @Project : springboot-demo
+ * @Author : zhangzongyuan
+ * @Description : [ TbWebSite ] 说明：无
+ * @Function :  功能说明：无
+ * @Date ：2021/12/23 17:49
+ * @Version ： 1.0
+ **/
+@Entity(name = "tb_web_link")
+@Data
+public class TbWebLink implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "link_id")
+    private Long linkId;
+
+    @Column(name = "page_id")
+    private Long pageId;
+
+    @Column(name = "link_no")
+    private String linkNo;
+
+    @Column(name = "link_href")
+    private String linkHref;
+
+    @Column(name = "link_name")
+    private String linkName;
+
+    @Column(name = "link_valid")
+    private String linkValid;
+
+    @Column(name = "img_tag")
+    private String imgTag;
+
+    @Column(name = "img_tag_attr")
+    private String imgTagAttr;
+
+    @Column(name = "pic_total")
+    private Long picTotal;
+
+     @Column(name = "add_time")
+    private String addTime;
+
+
+/*
+
+
+    @Entity标注保证实体能够被SpringBoot扫描到，对应表名为t_user。
+    @Id表明id。
+    @GeneratedValue中标注主键生成策略。
+    @Transient表示不需要映射的字段。
+
+
+GenerationType主键策略
+    TABLE： 使用一个特定的数据库表格来保存主键
+    SEQUENCE： 根据底层数据库的序列来生成主键，条件是数据库支持序列。这个值要与generator一起使用，generator 指定生成主键使用的生成器（可能是orcale中自己编写的序列）。
+    IDENTITY： 主键由数据库自动生成（主要是支持自动增长的数据库，如mysql）
+    AUTO： 主键由程序控制，也是GenerationType的默认值。
+
+
+ */
+}
---
+++ spring-boot-jsoup/src/main/java/cn/xiaocai/demo/jsoup/web/model/TbWebPicture.java
@@ -0,0 +1,70 @@
+package cn.xiaocai.demo.jsoup.web.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+/**
+ * @Project : springboot-demo
+ * @Author : zhangzongyuan
+ * @Description : [ TbWebSite ] 说明：无
+ * @Function :  功能说明：无
+ * @Date ：2021/12/23 17:49
+ * @Version ： 1.0
+ **/
+@Entity(name = "tb_web_pic")
+@Data
+public class TbWebPicture implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "pic_id")
+    private Long picId;
+
+    @Column(name = "link_id")
+    private String linkId;
+
+    @Column(name = "pic_href")
+    private String picHref;
+
+    @Column(name = "pic_name")
+    private String picName;
+
+    @Column(name = "pic_desc")
+    private String picDesc;
+
+    @Column(name = "pic_suffix")
+    private String picSuffix;
+
+    @Column(name = "pic_type")
+    private String picType;
+
+    @Column(name = "pic_no")
+    private Long picNo;
+
+    @Column(name = "add_time")
+    private String addTime;
+
+    @Column(name = "pic_content")
+    private Byte[] picContent;
+/*
+
+
+    @Entity标注保证实体能够被SpringBoot扫描到，对应表名为t_user。
+    @Id表明id。
+    @GeneratedValue中标注主键生成策略。
+    @Transient表示不需要映射的字段。
+
+
+GenerationType主键策略
+    TABLE： 使用一个特定的数据库表格来保存主键
+    SEQUENCE： 根据底层数据库的序列来生成主键，条件是数据库支持序列。这个值要与generator一起使用，generator 指定生成主键使用的生成器（可能是orcale中自己编写的序列）。
+    IDENTITY： 主键由数据库自动生成（主要是支持自动增长的数据库，如mysql）
+    AUTO： 主键由程序控制，也是GenerationType的默认值。
+
+
+ */
+}
