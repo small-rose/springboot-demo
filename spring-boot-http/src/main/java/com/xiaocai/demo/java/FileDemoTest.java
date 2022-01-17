0a26436691b675ab6f61cae8be8df4fb8b5a4e20
small-rose <small-rose@qq.com>
2022-01-17 20:23:50 +0800

    添加测试

--- spring-boot-http/src/main/java/com/xiaocai/demo/java/FileDemoTest.java
+++ spring-boot-http/src/main/java/com/xiaocai/demo/java/FileDemoTest.java
@@ -5,7 +5,9 @@ import org.junit.Test;
 
 import java.io.*;
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
 
@@ -110,6 +112,122 @@ public class FileDemoTest {
                         amout = amout.add(new BigDecimal(sr[4]));
                     }
                     result.put(file.getName(), amout.toString());
+                } catch(Exception e) {
+                    log.info("对账文件目录未获取到文件......");
+                    e.printStackTrace();
+                    throw new RuntimeException("对账文件目录"+filepath+"未获取到文件");
+                }finally {
+                    try {
+                        if (br!=null){
+                            br.close();
+                        }
+                        if (isr!=null){
+                            isr.close();
+                        }
+                    } catch (IOException e) {
+                    }
+                }
+            }
+
+            log.info("result {}", result);
+            result.forEach((key,value) -> {
+                System.out.println("fileName : "+ key +", Amount : "+ value);
+            });
+        }
+    }
+
+    String[] fileExclude = new String[] {"--"};
+    @Test
+    public void TestFilter() throws IOException{
+
+        List<File> allHandFiles = new ArrayList<File>();
+        File f1 = new File("D:\\当前任务文件夹\\安诚\\对账文件\\工行\\143012022010901.txt");
+        File f2 = new File("D:\\当前任务文件夹\\安诚\\对账文件\\工行\\1430120220110BAOQUAN.txt");
+        allHandFiles.add(f1);
+        allHandFiles.add(f2);
+        List<File> result = fileExcludeHandle(allHandFiles);
+       log.info("result {}", result);
+    }
+
+
+    private List<File> fileExcludeHandle(List<File> allHandFiles) {
+
+        if (allHandFiles==null || allHandFiles.size()==0 || fileExclude==null || fileExclude.length==0){
+            return allHandFiles;
+        }
+        List<File> rightList = new ArrayList<File>();
+        String fileName = "";
+        boolean fileFlag = false;
+        for (File file : allHandFiles){
+
+            fileName = file.getName();
+            System.out.println(fileName);
+            for (String excludeKey : fileExclude){
+                if (fileName.contains(excludeKey)){
+                    fileFlag = true ;
+                    break;
+                }
+            }
+            if (fileFlag){
+                continue;
+            }
+            rightList.add(file);
+        }
+        return rightList ;
+    }
+
+
+
+
+    @Test
+    public void filePolicyNo(){
+        String filepath = "D:\\当前任务文件夹\\安诚\\对账文件\\工行";
+        String filterPattern = "143012022";
+        File tmp = new File(filepath);
+        File[] files = tmp.listFiles(new FileFilter(){
+            @Override
+            public boolean accept(File pathname) {
+                Pattern pattern = Pattern.compile("^" + filterPattern +"[\\s\\S]*.txt");
+                log.info("pattern" + pattern.pattern());
+                log.info("pathname" + !pathname.isDirectory());
+                log.info("pathname" + pathname.getName());
+                log.info("matches" + pattern.matcher(pathname.getName()).matches());
+                return !pathname.isDirectory() && pattern.matcher(pathname.getName()).matches();
+            }
+        });
+        if (files==null || files.length == 0){
+            log.info("File Not Found !");
+        }else{
+            BufferedReader br = null;
+            InputStreamReader isr = null;
+
+            Map<String, String> result = new HashMap<>();
+            String line = "";
+            BigDecimal amout = null;
+            List<String> policyNo = new ArrayList<>();
+            for (File file : files){
+                try {
+                    amout = new BigDecimal("0.00");
+                    log.info("fileName : "+ file.getName());
+                    //上送文件为空时直接删除
+
+                    if (file.length() == 0) {
+                        //file.delete();
+                        log.info(filepath + "文件是空的,不直接删除");
+                        //return dataList;
+                        continue ;
+                    }
+
+
+                    isr = new InputStreamReader(new FileInputStream(file), "GBK");
+                    br = new BufferedReader(isr);
+                    while ((line = br.readLine()) != null) {
+                        //dataList.add(line);
+
+                        String[] sr = line.split("\\|");
+                        policyNo.add("'"+sr[3]+"'");
+                    }
+
                 } catch(Exception e) {
                     log.info("对账文件目录未获取到文件......");
                     e.printStackTrace();
--- spring-boot-http/src/main/java/com/xiaocai/demo/java/HttpNetDemoTest.java
+++ spring-boot-http/src/main/java/com/xiaocai/demo/java/HttpNetDemoTest.java
@@ -1,25 +1,55 @@
 package com.xiaocai.demo.java;
 
 import com.xiaocai.demo.http.HttpNetUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Project : springboot-demo
  * @Author : Small-Rose / zhangxaiocai
  * @Description : [ HttpNetDemoTest ] 说明：无
- * @Function :  功能说明：无
+ * @Function :  功能说明：
+ *  SchedualedThreadPoolExecutor 接收SchduledFutureTask类型的任务，是线程池调度任务的最小单位，有三种提交任务的方式：
+ *
+ *      schedule：延迟多长时间之后只执行一次；
+ *      scheduledAtFixedRate：延迟指定时间后执行一次，之后按照固定的时长周期执行；
+ *      scheduledWithFixedDelay：延迟指定时间后执行一次，之后按照：上一次任务执行时长 + 周期的时长 的时间去周期执行；
+ *
  * @Date ：2022/1/13 18:01
  * @Version ： 1.0
  **/
+@Slf4j
 public class HttpNetDemoTest {
+
+
 
     @Test
     public void http(){
-        String url = "https://www.jianshu.com/";
+        int threadNumber = 100 ;
+        final CountDownLatch count = new CountDownLatch(threadNumber);//参数为线程个数
+        ScheduledExecutorService pool = Executors.newScheduledThreadPool(5);
+        String url = "https://www.jianshu.com/p/506dac547b25";
         String referer = "https://www.jianshu.com/";
         HttpNetUtil http = new HttpNetUtil();
-        //http.httpjsonKillIp(url, referer,visitIP);
+        String resp  = http.httpGet(url, referer, null);
+        //log.info(resp);
+        Thread task = new Thread(() -> {
+            http.httpGet(url, referer, null);
+            log.info("----get url --- ");
+            //TimeUnit.SECONDS.sleep(2);
+            count.countDown();
+        });
+        pool.scheduleAtFixedRate(task, 5, 5, TimeUnit.SECONDS);
+        try {
 
-
+            count.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
