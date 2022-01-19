2d59bd76f421f5326bf708356aeb7f3c7ff19024
small-rose <small-rose@qq.com>
2022-01-19 09:29:48 +0800

    fastjson 版本升级

--- spring-boot-http/src/main/java/com/xiaocai/demo/http/HttpClientUtil.java
+++ spring-boot-http/src/main/java/com/xiaocai/demo/http/HttpClientUtil.java
@@ -2,6 +2,7 @@ package com.xiaocai.demo.http;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -12,7 +13,6 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
-import sun.net.www.http.HttpClient;
 
 import java.nio.charset.Charset;
 import java.util.ArrayList;
