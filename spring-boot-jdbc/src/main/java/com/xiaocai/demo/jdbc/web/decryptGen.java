91a40c830ee9d2ba1d187c8ff3db8bc2c98b55bb
small-rose <small-rose@qq.com>
2022-02-18 17:15:46 +0800

    druid密码加解密工具类

---
+++ spring-boot-jdbc/src/main/java/com/xiaocai/demo/jdbc/web/decryptGen.java
@@ -0,0 +1,17 @@
+package com.fndsoft.mm.boot;
+
+import com.alibaba.druid.filter.config.ConfigTools;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+
+public class decryptGen {
+    public static void main(String[] args) throws Exception {
+        String password = "123456";
+        String[] arr = ConfigTools.genKeyPair(512);
+        System.out.println(arr[1]);
+
+        String encryptPassword = ConfigTools.encrypt(arr[0], password);
+        System.out.println(encryptPassword);
+    }
+}
