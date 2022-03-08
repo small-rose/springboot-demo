88097a3a404a7785fb5b283f09ede27c728f67ae
small-rose <small-rose@qq.com>
2022-03-08 10:48:21 +0800

    修改readme

---
+++ spring-boot-spider/src/test/java/cn/xiaocai/demo/spider/SpringBootSpiderApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.xiaocai.demo.spider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSpiderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
