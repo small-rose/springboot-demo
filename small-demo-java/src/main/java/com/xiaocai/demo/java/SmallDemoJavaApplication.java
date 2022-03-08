5c43ac1502c994ca185ab925d2d22e2efe24bfbc
small-rose <small-rose@qq.com>
2022-03-08 10:07:03 +0800

    java基础Demo库

---
+++ small-demo-java/src/main/java/com/xiaocai/demo/java/SmallDemoJavaApplication.java
@@ -0,0 +1,13 @@
+package com.xiaocai.demo.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallDemoJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmallDemoJavaApplication.class, args);
+    }
+
+}
