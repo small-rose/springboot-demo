6782238efc42eea5077dc79768047d54e63f8402
small-rose <small-rose@qq.com>
2022-04-24 10:23:10 +0800

    更新kingbase配置

---
+++ spring-boot-kingbase/src/test/java/cn/xiaocai/demo/kingbase/test/KingbaseTest.java
@@ -0,0 +1,24 @@
+package cn.xiaocai.demo.kingbase.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @description: TODO 功能角色说明：
+ * TODO 描述：
+ * @author: 张小菜
+ * @date: 2022/4/24 10:07
+ * @version: v1.0
+ */
+
+@RunWith( SpringRunner.class)
+@SpringBootTest
+public class KingbaseTest {
+
+    @Test
+    public void test01(){
+
+    }
+}
