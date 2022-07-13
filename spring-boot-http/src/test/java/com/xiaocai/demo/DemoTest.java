8f9458d7c03bb09f3d1d1017fac5617d98963939
small-rose <small-rose@qq.com>
2022-07-13 18:50:15 +0800

    修正配置

--- spring-boot-http/src/test/java/com/xiaocai/demo/DemoTest.java
+++ spring-boot-http/src/test/java/com/xiaocai/demo/DemoTest.java
@@ -7,6 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * @Project : springboot-demo
  * @Author : small-rose/zhangxiaocai
@@ -25,5 +29,20 @@ public class DemoTest {
     @Test
     public void test01(){
         demoService.hello();
+    }
+
+    @Test
+    public void test02(){
+        Integer a = 1000 , b =1000;
+        Integer c = 100 , d = 100;
+
+        System.out.println(a==b);
+        System.out.println(c==d);
+        System.out.println(Integer.MAX_VALUE);
+
+
+        List<String> collect = Stream.of("1,2,3".split(",")).collect(Collectors.toList());
+        System.out.println(collect);
+
     }
 }
