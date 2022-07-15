914e2cf04da8564c615f094e6a31858dba6bbe40
small-rose <small-rose@qq.com>
2022-07-15 19:45:29 +0800

    KingBase多数据源-分包模式试环境

---
+++ spring-boot-kingbase/src/main/java/cn/xiaocai/demo/kingbase/web/mapper/kingbase/TestMapper.java
@@ -0,0 +1,10 @@
+package cn.xiaocai.demo.kingbase.web.mapper.kingbase;
+
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TestMapper {
+
+    public Long test();
+}
--- spring-boot-kingbase/src/test/java/cn/xiaocai/demo/kingbase/test/KingbaseTest.java
+++ spring-boot-kingbase/src/test/java/cn/xiaocai/demo/kingbase/test/KingbaseTest.java
@@ -1,7 +1,9 @@
 package cn.xiaocai.demo.kingbase.test;
 
+import cn.xiaocai.demo.kingbase.web.mapper.kingbase.TestMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -14,11 +16,15 @@ import org.springframework.test.context.junit4.SpringRunner;
  */
 
 @RunWith( SpringRunner.class)
-@SpringBootTest
+@SpringBootTest()
 public class KingbaseTest {
+
+
+    @Autowired
+    TestMapper testMapper;
 
     @Test
     public void test01(){
-
+        testMapper.test();
     }
 }
