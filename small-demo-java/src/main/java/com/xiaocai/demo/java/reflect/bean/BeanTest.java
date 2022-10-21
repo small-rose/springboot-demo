d47073042fc2fd992b43b025eb14fedb96636c6d
small-rose <small-rose@qq.com>
2022-10-21 18:34:28 +0800

    final值修改

---
+++ small-demo-java/src/main/java/com/xiaocai/demo/java/reflect/bean/BeanTest.java
@@ -0,0 +1,21 @@
+package com.xiaocai.demo.java.reflect.bean;
+
+import java.io.Serializable;
+
+/**
+ * @Project : springboot-demo
+ * @Author : Small-Rose / zhangxaiocai
+ * @Description : [ BeanTest ] 说明：无
+ * @Function :  功能说明：无
+ * @Date ：2022/10/17 13:48
+ * @Version ： 1.0
+ **/
+public class BeanTest implements Serializable {
+
+    private static final Long serialVersionUID = 100L ;
+
+
+    public long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+}
--- small-demo-java/src/test/java/com/xiaocai/demo/java/reflect/ObjectUtilTest.java
+++ small-demo-java/src/test/java/com/xiaocai/demo/java/reflect/ObjectUtilTest.java
@@ -1,7 +1,11 @@
 package com.xiaocai.demo.java.reflect;
 
+import com.xiaocai.demo.java.reflect.bean.BeanTest;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 
 public class ObjectUtilTest {
 
@@ -17,5 +21,37 @@ public class ObjectUtilTest {
         String name = "XiaoCai";
         Object clazz = objectUtil.getObject(name);
 
+    }
+
+    @Test
+    public void test_23() {
+        try{
+            Class aClass = BeanTest.class;
+
+            BeanTest o = (BeanTest) aClass.newInstance();
+            Field declaredField = aClass.getDeclaredField("serialVersionUID");
+            //获取modifiers属性，modifiers属性也是私有的
+            Field modifiers = Field.class.getDeclaredField("modifiers");
+            // 其他特性如 private, static 保持不变
+            modifiers.setAccessible(true);
+            modifiers.setInt(declaredField,declaredField.getModifiers() & ~Modifier.FINAL);
+
+            declaredField.setAccessible(true);
+            declaredField.set(o, new Long(2000L));
+
+            System.out.println(o.getSerialVersionUID());
+
+
+            /**
+             * 内省机制
+             */
+            //PropertyDescriptor propertyDescriptor = new PropertyDescriptor("serialVersionUID", aClass);
+            //Method setMethod = propertyDescriptor.getWriteMethod();
+            //setMethod.invoke(o, 200L);
+            //System.out.println(o.getSerialVersionUID());
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
